1f83e999f5cf03834e874391d06ea681d1f1b022
jsharp4 <jasharp2000@gmail.com>
2019-02-27 15:17:05 -0600

    fixed naming errors

--- hw/hw5/hw5_1/decoder_3_8.v
+++ hw/hw5/hw5_1/decoder_3_8.v
@@ -2,24 +2,26 @@ module decoder_3_8(in, out, en);
 
     input en;
     input [2:0] in;
-    output reg [7:0] out;
+    output [7:0] out;
+
+    reg [7:0] out_if_en;
 
     always @(*) begin
         out = 8'h00;
-        if (en) begin
-            case (in)
-                3'b000  : out = 8'h01;
-                3'b001  : out = 8'h02;
-                3'b010  : out = 8'h04;
-                3'b011  : out = 8'h08;
-                3'b100  : out = 8'h10;
-                3'b101  : out = 8'h20;
-                3'b110  : out = 8'h40;
-                3'b111  : out = 8'h80;
-                default : $error("DEFAULT CASE FOR decoder_3_8");
-            endcase
-        end
+        case (in)
+            3'b000  : out_if_en = 8'h01;
+            3'b001  : out_if_en = 8'h02;
+            3'b010  : out_if_en = 8'h04;
+            3'b011  : out_if_en = 8'h08;
+            3'b100  : out_if_en = 8'h10;
+            3'b101  : out_if_en = 8'h20;
+            3'b110  : out_if_en = 8'h40;
+            3'b111  : out_if_en = 8'h80;
+            default : $error("DEFAULT CASE FOR decoder_3_8");
+        endcase
     end
+
+    out = (en) ? out_if_en : 8'h00;
 
 endmodule
 
